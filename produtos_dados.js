6d267ef58e6c40711038ef92342292db67da1b2a
Roger Tang <roger.tang.rogertang@gmail.com>
2026-05-01 11:42:22 -0300

    inserção de camisa de pesca

--- produtos_dados.js
+++ produtos_dados.js
@@ -1,92 +1,99 @@
 const listaProdutos = [
-  {
-    "nome": "Camisa Japonesa",
-    "preco": 23.3,
-    "principal": "download.webp",
-    "detalhes": [
-      "download_detalhe1.webp",
-      "download_detalhe2.webp",
-      "download_detalhe3.webp"
-    ]
-  },
-  {
-    "nome": "Camiseta Básica Preta",
-    "preco": 49.9,
-    "principal": "Modelo1.jfif",
-    "detalhes": [
-      "Modelo1_detalhe1.jfif",
-      "Modelo1_detalhe2.jfif",
-      "Modelo1_detalhe3.jfif"
-    ]
-  },
-  {
-    "nome": "Camiseta Azul Premium",
-    "preco": 59.9,
-    "principal": "modelo2.jfif",
-    "detalhes": [
-      "modelo2_detalhe1.jfif",
-      "modelo2_detalhe2.jfif",
-      "modelo2_detalhe3.jfif"
-    ]
-  },
-  {
-    "nome": "Camiseta Vermelha",
-    "preco": 69.9,
-    "principal": "modelo3.jfif",
-    "detalhes": [
-      "modelo3_detalhe1.jfif",
-      "modelo3_detalhe2.jfif",
-      "modelo3_detalhe3.jfif"
-    ]
-  },
-  {
-    "nome": "Jesus is the Hope os the world",
-    "preco": 11.0,
-    "principal": "Jesus.webp",
-    "detalhes": [
-      "Jesus_detalhe1.webp",
-      "Jesus_detalhe2.webp",
-      "Jesus_detalhe3.webp"
-    ]
-  },
-  {
-    "nome": "Gato",
-    "preco": 3.0,
-    "principal": "Gato.webp",
-    "detalhes": [
-      "Gato_detalhe1.webp",
-      "Gato_detalhe2.webp",
-      "Gato_detalhe3.webp"
-    ]
-  },
-  {
-    "nome": "Dinosauro",
-    "preco": 5.5,
-    "principal": "Dinosauro.webp",
-    "detalhes": [
-      "Dinosauro_detalhe1.webp",
-      "Dinosauro_detalhe2.webp",
-      "Dinosauro_detalhe3.webp"
-    ]
-  },
-  {
-    "nome": "Camiseta Feminina",
-    "preco": 13.0,
-    "principal": "feminina.webp",
-    "detalhes": [
-      "feminina_detalhe1.webp",
-      "feminina_detalhe2.webp",
-      "feminina_detalhe3.webp"
-    ]
-  },
-  {
-    "nome": "Pink Floyd",
-    "preco": 33.0,
-    "principal": "Pink Floyd.webp",
-    "detalhes": [
-      "Pink Floyd_detalhe1.webp",
-      "Pink Floyd_detalhe2.webp",
-      "Pink Floyd_detalhe3.webp"
-    ]
-  }
+    {
+        "nome":  "Camisa Japonesa",
+        "preco":  23.3,
+        "principal":  "download.webp",
+        "detalhes":  [
+                         "download_detalhe1.webp",
+                         "download_detalhe2.webp",
+                         "download_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "Camiseta Básica Preta",
+        "preco":  49.9,
+        "principal":  "Modelo1.jfif",
+        "detalhes":  [
+                         "Modelo1_detalhe1.jfif",
+                         "Modelo1_detalhe2.jfif",
+                         "Modelo1_detalhe3.jfif"
+                     ]
+    },
+    {
+        "nome":  "Camiseta Azul Premium",
+        "preco":  59.9,
+        "principal":  "modelo2.jfif",
+        "detalhes":  [
+                         "modelo2_detalhe1.jfif",
+                         "modelo2_detalhe2.jfif",
+                         "modelo2_detalhe3.jfif",
+                         "modelo2_detalhe4.jfif"
+                     ]
+    },
+    {
+        "nome":  "Camiseta Vermelha",
+        "preco":  69.9,
+        "principal":  "modelo3.jfif",
+        "detalhes":  [
+                         "modelo3_detalhe1.jfif",
+                         "modelo3_detalhe2.jfif",
+                         "modelo3_detalhe3.jfif"
+                     ]
+    },
+    {
+        "nome":  "Jesus is the Hope os the world",
+        "preco":  11.0,
+        "principal":  "Jesus.webp",
+        "detalhes":  [
+                         "Jesus_detalhe1.webp",
+                         "Jesus_detalhe2.webp",
+                         "Jesus_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "Gato",
+        "preco":  3.0,
+        "principal":  "Gato.webp",
+        "detalhes":  [
+                         "Gato_detalhe1.webp",
+                         "Gato_detalhe2.webp",
+                         "Gato_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "Dinosauro",
+        "preco":  5.5,
+        "principal":  "Dinosauro.webp",
+        "detalhes":  [
+                         "Dinosauro_detalhe1.webp",
+                         "Dinosauro_detalhe2.webp",
+                         "Dinosauro_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "Camiseta Feminina",
+        "preco":  13.0,
+        "principal":  "feminina.webp",
+        "detalhes":  [
+                         "feminina_detalhe1.webp",
+                         "feminina_detalhe2.webp",
+                         "feminina_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "Pink Floyd",
+        "preco":  33.0,
+        "principal":  "Pink Floyd.webp",
+        "detalhes":  [
+                         "Pink Floyd_detalhe1.webp",
+                         "Pink Floyd_detalhe2.webp",
+                         "Pink Floyd_detalhe3.webp"
+                     ]
+    },
+    {
+        "nome":  "pesca",
+        "preco":  21.9,
+        "principal":  "pesca.webp",
+        "detalhes":  null
+    }
 ];
